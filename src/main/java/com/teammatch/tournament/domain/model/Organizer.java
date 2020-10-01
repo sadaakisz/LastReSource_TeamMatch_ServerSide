f0d297fb55b85ee32244abb3f4f4a95ea28e3de7
Sergio Alva <sergio_alva99@hotmail.com>
2020-09-30 22:30:18 -0500

    Organizer model
    Implementacion de Organizer - model

--- src/main/java/com/teammatch/tournament/domain/model/Organizer.java
+++ src/main/java/com/teammatch/tournament/domain/model/Organizer.java
@@ -1,4 +1,50 @@
 package com.teammatch.tournament.domain.model;
 
-public class Organizer {
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "organizers")
+public class Organizer extends AuditModel {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(unique = true)
+    private String username;
+
+    @NotNull
+    private String password;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public Organizer setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Organizer setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Organizer setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
 }
