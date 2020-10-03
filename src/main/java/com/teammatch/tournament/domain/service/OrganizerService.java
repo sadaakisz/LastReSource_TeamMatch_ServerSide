b5453a7171c8f0dc7edeb6ab71dcc346d03318d9
sadaakisz <hssm2000.sadaaki@gmail.com>
2020-10-03 08:50:10 -0500

    Created organizer service interface.
    Created organizer service interface.

--- src/main/java/com/teammatch/tournament/domain/service/OrganizerService.java
+++ src/main/java/com/teammatch/tournament/domain/service/OrganizerService.java
@@ -1,4 +1,14 @@
 package com.teammatch.tournament.domain.service;
 
+import com.teammatch.tournament.domain.model.Organizer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
 public interface OrganizerService {
+    Page<Organizer> getAllOrganizers(Pageable pageable);
+    Organizer getOrganizerById(Long organizerId);
+    Organizer createOrganizer(Organizer organizer);
+    Organizer updateOrganizer(Long organizerId, Organizer organizer);
+    ResponseEntity<?> deleteOrganizer(Long organizerId);
 }
