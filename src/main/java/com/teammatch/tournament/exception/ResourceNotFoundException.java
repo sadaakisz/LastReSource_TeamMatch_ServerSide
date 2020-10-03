3992888989ef6507aab54da894650c141fdb774e
erick <erickquiber@gmail.com>
2020-10-03 10:17:37 -0500

    new changes

--- src/main/java/com/teammatch/tournament/exception/ResourceNotFoundException.java
+++ src/main/java/com/teammatch/tournament/exception/ResourceNotFoundException.java
@@ -20,6 +20,8 @@ public class ResourceNotFoundException extends RuntimeException{
     public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
         super(String.format("Resource %s not found for %s with value %s",
                 resourceName, fieldName, fieldValue));
+
+        //exception
     }
 
 }
