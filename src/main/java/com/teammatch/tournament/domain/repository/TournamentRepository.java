1872862c0c3cc0b0205dd84b9c0f4d177fafde9d
JoseMoreno21 <57198383+JoseMoreno21@users.noreply.github.com>
2020-10-02 22:54:14 -0500

    Update TournamentRepository.java
    Tournament Repository

--- src/main/java/com/teammatch/tournament/domain/repository/TournamentRepository.java
+++ src/main/java/com/teammatch/tournament/domain/repository/TournamentRepository.java
@@ -1,4 +1,12 @@
 package com.teammatch.tournament.domain.repository;
+import com.teammatch.tournament.domain.model.Tournament;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 public interface TournamentRepository {
+    Page<Tournament> findByOrganizerId(Long organizerId, Pageable pageable);
+    Optional<Tournament> findByIdAndOrganizerId(Long id, Long organizerId);
 }
