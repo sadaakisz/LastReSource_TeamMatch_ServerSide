19c6985b56b793feca5cafb941b4fb1af969590f
erick <erickquiber@gmail.com>
2020-10-03 12:26:56 -0500

    new changes in properties

--- src/test/java/com/teammatch/tournament/TournamentApplicationTests.java
+++ src/test/java/com/teammatch/tournament/TournamentApplicationTests.java
@@ -1,9 +1,12 @@
 package com.teammatch.tournament;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes =TournamentApplication.class )
 class TournamentApplicationTests {
 
     @Test
