631de2a257b05bb5d5871d8fa17e3dd479fb2c8b
sadaakisz <hssm2000.sadaaki@gmail.com>
2020-10-15 17:07:41 -0500

    Created Profile model.
    Created Profile model.

---
+++ src/main/java/com/teammatch/tournament/domain/model/Profile.java
@@ -0,0 +1,125 @@
+package com.teammatch.tournament.domain.model;
+
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+@Table(name = "profiles")
+public class Profile extends AuditModel{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    @Size(max = 100)
+    @NaturalId
+    @Column(unique = true)
+    private String username;
+
+    @Lob
+    private String description;
+
+    @NotNull
+    private String gender;
+
+    @NotNull
+    @Size(max = 100)
+    private String emailAddress;
+
+    @NotNull
+    private String phoneNumber;
+
+    @NotNull
+    private Date birthDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Profile setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Profile setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Profile setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Profile setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Profile setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Profile setGender(String gender) {
+        this.gender = gender;
+        return this;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public Profile setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+        return this;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Profile setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public Profile setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+        return this;
+    }
+}
