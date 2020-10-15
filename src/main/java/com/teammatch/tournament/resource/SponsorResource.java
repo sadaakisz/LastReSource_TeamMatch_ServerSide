7103595990883fde0b14434abcbb9837b73439a2
Sergio Alva <sergio_alva99@hotmail.com>
2020-10-15 17:56:19 -0500

    Create SponsorResource.java

---
+++ src/main/java/com/teammatch/tournament/resource/SponsorResource.java
@@ -0,0 +1,38 @@
+package com.teammatch.tournament.resource;
+
+import com.teammatch.tournament.domain.model.AuditModel;
+
+public class SponsorResource extends AuditModel {
+
+    private Long id;
+    private String name;
+    private String url;
+
+    public Long getId() {
+        return id;
+    }
+
+    public SponsorResource setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public SponsorResource setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public SponsorResource setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+
+}
